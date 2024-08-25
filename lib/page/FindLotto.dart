7ef4dd1c18cdbf9da526eb726f9277d38a5d7958
BobBieNeno <bie007x@gmail.com>
2024-08-25 19:31:57 +0700

    set statefull

---
+++ lib/page/FindLotto.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FindlottoPage extends StatefulWidget {
+  const FindlottoPage({super.key});
+
+  @override
+  State<FindlottoPage> createState() => _FindlottoPageState();
+}
+
+class _FindlottoPageState extends State<FindlottoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("findLotto"),
+      ),
+      body: Column(),
+    );
+  }
+}
