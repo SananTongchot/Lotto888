d29dea8f1a69261340727a1a0f316fb25431a3d3
BobBieNeno <bie007x@gmail.com>
2024-09-10 13:19:18 +0700

    set state

---
+++ lib/page/FindLotto.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FineLottoPage extends StatefulWidget {
+  const FineLottoPage({super.key});
+
+  @override
+  State<FineLottoPage> createState() => _FineLottoPageState();
+}
+
+class _FineLottoPageState extends State<FineLottoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        
+      ),
+    );
+  }
+}
