06cb43e1a3722b804ddeab40eff5a4f1ea1ff9da
PeerapatKaewdee <136581789+PeerapatKaewdee@users.noreply.github.com>
2024-09-11 15:07:39 +0700

    connect

---
+++ lib/model/request/UserPostRegisterReq.dart
@@ -0,0 +1,37 @@
+// To parse this JSON data, do
+//
+//     final userRegisterPostRequest = userRegisterPostRequestFromJson(jsonString);
+
+import 'dart:convert';
+
+UserRegisterPostRequest userRegisterPostRequestFromJson(String str) => UserRegisterPostRequest.fromJson(json.decode(str));
+
+String userRegisterPostRequestToJson(UserRegisterPostRequest data) => json.encode(data.toJson());
+
+class UserRegisterPostRequest {
+    String username;
+    String phone;
+    String email;
+    String password;
+
+    UserRegisterPostRequest({
+        required this.username,
+        required this.phone,
+        required this.email,
+        required this.password,
+    });
+
+    factory UserRegisterPostRequest.fromJson(Map<String, dynamic> json) => UserRegisterPostRequest(
+        username: json["username"],
+        phone: json["phone"],
+        email: json["email"],
+        password: json["password"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "username": username,
+        "phone": phone,
+        "email": email,
+        "password": password,
+    };
+}
--- lib/page/logInpage.dart
+++ lib/page/logInpage.dart
@@ -122,7 +122,7 @@ class _LoginPageState extends State<LoginPage> {
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                    builder: (context) => const Registerpage(), // Update if necessary
+                                    builder: (context) => RegisterPage(), // Update if necessary
                                   ),
                                 );
                               },
