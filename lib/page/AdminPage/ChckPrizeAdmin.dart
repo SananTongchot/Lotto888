1685208499285c8fa76c71ffca28d4bebb31871c
PeerapatKaewdee <136581789+PeerapatKaewdee@users.noreply.github.com>
2024-09-13 15:49:23 +0700

    random

--- lib/page/AdminPage/ChckPrizeAdmin.dart
+++ lib/page/AdminPage/ChckPrizeAdmin.dart
@@ -1,22 +1,41 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/config/config.dart';
+import 'package:http/http.dart' as http; // ใช้สำหรับเรียกใช้งาน HTTP
+import 'dart:convert'; // ใช้สำหรับจัดการกับ JSON
 
 class CheckPrizeAdmin extends StatefulWidget {
-  int idx = 0;
-   CheckPrizeAdmin({super.key , required this.idx});
+  final int idx;
+  CheckPrizeAdmin({super.key, required this.idx});
 
   @override
   State<CheckPrizeAdmin> createState() => _CheckPrizeAdminState();
 }
 
 class _CheckPrizeAdminState extends State<CheckPrizeAdmin> {
+  String url = ""; // ตัวแปรสำหรับเก็บ URL
+  List<String> prizes = []; // ตัวแปรสำหรับเก็บรางวัล
+
   @override
+  void initState() {
+    super.initState();
+    // ดึง URL จาก Configuration
+    Configuration.getConfig().then((config) {
+      setState(() {
+        url = config['apiEndpoint'] ?? "";
+      });
+    });
+  }
+
  
- @override
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         iconTheme: const IconThemeData(
-          color: Colors.white, // กำหนดให้ไอคอนเป็นสีขาว
+          color: Colors.white,
         ),
         title: const Text("Check"),
         centerTitle: true,
@@ -28,7 +47,7 @@ class _CheckPrizeAdminState extends State<CheckPrizeAdmin> {
         ),
         actions: [
           IconButton(
-            icon: const Icon(Icons.person), // ไอคอนรูปคน
+            icon: const Icon(Icons.person),
             onPressed: () {
               // การทำงานเมื่อกดไอคอนรูปคน
             },
@@ -68,118 +87,115 @@ class _CheckPrizeAdminState extends State<CheckPrizeAdmin> {
           ],
         ),
       ),
-     body: Column(
-  children: [
-    Expanded(
-      flex: 2, // ขนาดของครึ่งบน
-      child: ClipRRect(
-        borderRadius: const BorderRadius.only(
-          bottomLeft: Radius.circular(30), // การโค้งมนที่มุมล่างซ้าย
-          bottomRight: Radius.circular(30), // การโค้งมนที่มุมล่างขวา
-        ),
-        child: Container(
-          color: const Color(0xFF2445EF), // สีของ Container ในครึ่งบน
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                const Text(
-                  'รางวัลที่ 1',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const Text(
-                                '123456', // แสดงตัวเลข 6 หลัก
-                                style: TextStyle(
-                                  fontSize: 30,
-                                    color: Colors.white,
-                                  fontWeight: FontWeight.bold, // ทำให้ตัวเลขเด่นขึ้น
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
+              child: Container(
+                color: const Color(0xFF2445EF),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'รางวัลที่ 1',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Text(
+                        '123456',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Expanded(
+                        child: GridView.builder(
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 3.0,
+                            mainAxisSpacing: 16.0,
+                            childAspectRatio: 2.0,
+                          ),
+                          itemCount: prizes.length, // จำนวนการ์ด
+                          itemBuilder: (context, index) {
+                            return Card(
+                              elevation: 5,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(30),
+                                  bottom: Radius.circular(20),
                                 ),
                               ),
-                const SizedBox(height: 8), // ระยะห่างระหว่างข้อความและ GridView
-                Expanded(
-                  child: GridView.builder(
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2, // 2 คอลัมน์
-                      crossAxisSpacing: 3.0, // ระยะห่างระหว่างคอลัมน์
-                      mainAxisSpacing: 16.0, // ระยะห่างระหว่างแถว
-                      childAspectRatio: 2.0, // อัตราส่วนขนาดของ Card (กว้างยาว)
-                    ),
-                    itemCount: 4, // จำนวนการ์ด
-                    itemBuilder: (context, index) {
-                      return Card(
-                        elevation: 5,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.vertical(
-                            top: Radius.circular(30), // การโค้งมนที่ขอบบน
-                            bottom: Radius.circular(20), // การโค้งมนที่ขอบล่าง
-                          ),
-                        ),
-                        child: Center(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min, // จัดเรียงข้อความให้ใช้พื้นที่น้อยที่สุด
-                            mainAxisAlignment: MainAxisAlignment.center, // จัดข้อความให้อยู่กึ่งกลาง
-                            children: [
-                              Text(
-                                'รางวัลที่ ${index + 2}',
-                                style: const TextStyle(fontSize: 16),
-                              ),
-                              const SizedBox(height: 8), // เพิ่มระยะห่างระหว่างข้อความและตัวเลข
-                              const Text(
-                                '123456', // แสดงตัวเลข 6 หลัก
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold, // ทำให้ตัวเลขเด่นขึ้น
+                              child: Center(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      'รางวัลที่ ${index + 1}',
+                                      style: const TextStyle(fontSize: 16),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      prizes.isNotEmpty ? prizes[index] : 'ไม่มีข้อมูล',
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                            ],
-                          ),
+                            );
+                          },
                         ),
-                      );
-                    },
+                      ),
+                    ],
                   ),
                 ),
-              ],
+              ),
             ),
           ),
-        ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.white,
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: (){
+                    _fetchRandomPrizes();
+                  }, // เรียกใช้งานฟังก์ชันเมื่อกดปุ่ม
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF2445EF),
+                    padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  child: const Text(
+                    'LOTTO888',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-    ),
-    Expanded(
-      flex: 3, // ขนาดของครึ่งล่าง
-child: Container(
-  color: Colors.white, // สีของ Container ในครึ่งล่าง
-  child: Center(
-    child: ElevatedButton(
-      onPressed: () {}, // ใส่ฟังก์ชันที่คุณต้องการเมื่อกดปุ่ม
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color(0xFF2445EF), // สีของปุ่ม
-        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20), // ขยายขนาดของปุ่ม
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15), // มุมปุ่มมน
-        ),
-      ),
-      child: const Text(
-        'LOTTO888',
-        style: TextStyle(
-          color: Colors.white, // สีของข้อความ
-          fontSize: 20,
-        ),
-      ),
-    ),
-  ),
-),
-
-
-
-
-    ),
-  ],
-),
-
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: const Color(0xFF2445EF),
         selectedItemColor: Colors.white,
@@ -209,4 +225,29 @@ child: Container(
       ),
     );
   }
+   Future<void> _fetchRandomPrizes() async {
+  if (url.isEmpty) {
+    // URL not set
+    return;
+  }
+
+  try {
+    final response = await http.get(Uri.parse('$url/random'));
+log(response.body);
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body) as Map<String, dynamic>;
+      setState(() {
+        prizes = List<String>.from(data['numbers'] ?? []);
+      });
+    } else {
+      throw Exception('Failed to fetch prizes');
+    }
+  } catch (e) {
+    // Handle errors
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Error: $e')),
+    );
+  }
+}
+
 }
